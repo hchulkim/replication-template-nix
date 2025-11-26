e8d8b7298f4cffda7821071c57486f3a1b258685
Hyoungchul Kim <168149812+hchulkim@users.noreply.github.com>
2025-11-26 02:09:50 -0500

    Create generate_env.R

---
+++ generate_env.R
@@ -0,0 +1,29 @@
+# -----------------------------------------------------------------------------
+# Initial date: 
+# Modified date: 
+# Maintainer: Hyoungchul Kim
+# Task: R script that creates nix/rix environment. You need to have here and rix R packages to run this. You can actually run it without having any R related packages and even R. If interested, read the vignettes of rix R package.
+# -----------------------------------------------------------------------------
+
+
+library(here)
+
+path_default_nix <- here() 
+
+library(rix)
+
+rix(date = "2025-11-24", # use `available_dates()` to get the snapshot date you want to use.
+# list R packages to install:
+    r_pkgs = c("rix", "languageserver", "pacman", "here", "data.table"),
+# list system packages to install:
+    system_pkgs = c("quarto", "git"),
+    git_pkgs = NULL,
+# list julia packages to install:
+    jl_conf = list(
+      jl_version = "1.11",
+      jl_pkgs = c("CSV", "DataFrames")
+      ),
+    ide = "none",
+    project_path = path_default_nix,
+    overwrite = TRUE,
+    print = TRUE)
