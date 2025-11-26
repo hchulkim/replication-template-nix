8d2dcdcbe6f3fee03acf2f9cd873cce85739afbf
Hyoungchul Kim <168149812+hchulkim@users.noreply.github.com>
2025-11-26 01:59:17 -0500

    Create example R script.
    This script loads necessary packages and prints a greeting.

---
+++ src/lorem.R
@@ -0,0 +1,11 @@
+# -----------------------------------------------------------------------------
+# Initial date: 
+# Modified date: 
+# Maintainer: Hyoungchul Kim
+# Task: Example R script
+# -----------------------------------------------------------------------------
+
+# download/load necessary packages
+pacman::p_load(here, data.table)
+
+print("Hello World!")
