01f3f366c22a2ea26a7e7f0b0c5cfd356f6f2a85
himakun <hkim.inbox@gmail.com>
2026-04-15 17:43:17 -0400

    setup nvim in nix environment.

--- generate_env.R
+++ generate_env.R
@@ -1,41 +1,19 @@
-# -----------------------------------------------------------------------------
-# Initial date: 2026-02-25
-# Modified date: 2026-02-25
-# Maintainer: Hyoungchul Kim
-# Task: R script that creates nix/rix environment. You need to have here and rix R packages to run this. You can actually run it without having any R related packages and even R. If interested, read the vignettes of rix R package.
-# -----------------------------------------------------------------------------
-
-# when using local machine with here R package
-if (require("here")) {
-    # when you have here R package in local R
-    library(here)
-    path_default_nix <- here() 
-} else {
-    # when using Docker
-    path_default_nix <- "."
-    }
-
 library(rix)
 
-rix(date = "2025-11-24", # use `available_dates()` to get the snapshot date you want to use.
-# list R packages to install:
-    r_pkgs = c("rix", "languageserver", "pacman", "here", "data.table"),
-# list system packages to install:
-    system_pkgs = c("quarto", "git"),
-    git_pkgs = NULL,
-# list python packages to install:
-    py_conf = list(
-        py_version = "3.12",
-        py_pkgs = c("numpy", "pandas", "pyarrow", "statsmodels", "matplotlib")
-    ),
-# list julia packages to install:
-    jl_conf = list(
-      jl_version = "1.11",
-      jl_pkgs = c("CSV", "DataFrames")
-      ),
-    ide = "none",
-    project_path = path_default_nix,
-    overwrite = TRUE,
-    print = TRUE)
-    # this shell_hook is not necessary unless you are using nvim-R
-    #shell_hook = "export R_LIBS=$HOME/R/library")
+rix(
+  r_ver = "4.4.2",
+  r_pkgs = c("dplyr", "ggplot2"),
+  system_pkgs = NULL,
+  git_pkgs = NULL,
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE,
+  shell_hook = "
+    # Make R.nvim's vendored nvimcom package available without writing
+    # to a global R library. One-time install:
+    #   R CMD INSTALL --library=.Rlib \\
+    #     ~/.local/share/nvim/lazy/R.nvim/nvimcom
+    # rix's .Rprofile strips R_LIBS_USER but leaves R_LIBS_SITE alone.
+    export R_LIBS_SITE=\"$PWD/.Rlib:$R_LIBS_SITE\"
+  "
+)
